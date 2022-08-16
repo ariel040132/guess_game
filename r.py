c628e3df39210c1a6926c36b5afcb26672b18375
Ariel Guo <ariel040132@gmail.com>
2022-08-16 18:25:55 +0800

    final version

--- r.py
+++ r.py
@@ -1,10 +1,13 @@
 import random
-
-r = random.randint(1, 100)
+start = input("請決定隨機數字範圍開始值：")
+end = input("請決定隨機數字範圍結束值：")
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 count = 0
 while True:
 	count = count + 1  # count += 1
-	num = input("請輸入1~100的隨機數：")
+	num = input("請輸入您心中所想的隨機數：")
 	num = int(num)
 	if num == r:
 		print("這是你猜的第", count, "次")
