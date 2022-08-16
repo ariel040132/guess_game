c6c8cbd73b5870d1330972c2752b9f6ea7963145
Ariel Guo <ariel040132@gmail.com>
2022-08-16 15:35:08 +0800

    extension

--- r.py
+++ r.py
@@ -1,15 +1,18 @@
 import random
 
 r = random.randint(1, 100)
-
+count = 0
 while True:
+	count = count + 1  # count += 1
 	num = input("請輸入1~100的隨機數：")
 	num = int(num)
 	if num == r:
+		print("這是你猜的第", count, "次")
 		print("恭喜，猜對了！")
 		break
 	elif num > r:
 		print("比答案大")
 	elif num < r:
 		print("比答案小")
+	print("這是你猜的第", count, "次")
 		
